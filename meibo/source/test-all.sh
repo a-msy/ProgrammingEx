ec54595cd82a3bdb51856b0ca6ec448bb3026a7f
a-msy <m5s2y5a@gmail.com>
2019-05-24 10:56:05 +0900

    Update test-all.sh

--- meibo/source/test-all.sh
+++ meibo/source/test-all.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 gcc -Wall -o my-command kimatu.c || exit 1
-./db-sample  < test_12.txt > result.log
-./my-command < test_12.txt > my-result.log
-diff -s result.log my-result.log
+./db-sample  < test_12.txt > result12.log
+./my-command < test_12.txt > my-result12.log
+diff -s result12.log my-result12.log
+./db-sample  < test_11.txt > result11.log
+./my-command < test_11.txt > my-result11.log
+diff -s result11.log my-result11.log
